5206e094dae4131045cc2207b52399081841b879
webmasterdevlin <webmasterdevlin@gmail.com>
2017-05-03 12:35:20 +0800

    First commit

---
+++ SimpleRestAndJson/SimpleRestAndJson/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using SimpleRestAndJson.Annotations;
+using SimpleRestAndJson.Models;
+
+namespace SimpleRestAndJson.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private string _station;
+
+        public string Station
+        {
+            get { return _station; }
+            set
+            {
+                _station = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private long _elevation;
+
+        public long Elevation
+        {
+            get { return _elevation; }
+            set
+            {
+                _elevation = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private long _temp;
+
+        public long Temp
+        {
+            get { return _temp; }
+            set
+            {
+                _temp = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private long _humidity;
+
+        public long Humidity
+        {
+            get { return _humidity; }
+            set
+            {
+                _humidity = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async Task GetWeatherAsync(string url)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            var response = await client.GetAsync(client.BaseAddress);
+            response.EnsureSuccessStatusCode();
+            var JsonResult = response.Content.ReadAsStringAsync().Result;
+            var weather = JsonConvert.DeserializeObject<WeatherResult>(JsonResult);
+            SetValues(weather);
+        }
+
+        private void SetValues(WeatherResult weather)
+        {
+            var stationName = weather.weatherObservation.stationName;
+            var elevation = weather.weatherObservation.elevation;
+            var temperature = weather.weatherObservation.temperature;
+            var humidity = weather.weatherObservation.humidity;
+
+            Station = stationName;
+            Elevation = elevation;
+            Temp = temperature;
+            Humidity = humidity;
+        }
+
+
+
+
+    }
+}
---
+++ SimpleRestAndJson/SimpleRestAndJson/Views/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleRestAndJson.ViewModels;
+using Xamarin.Forms;
+
+namespace SimpleRestAndJson
+{
+    public partial class MainPage : ContentPage
+    {
+        private MainPageViewModel vm;
+        public MainPage()
+        {
+            vm = new MainPageViewModel();
+            BindingContext = vm;
+
+            InitializeComponent();
+        }
+
+        private async void Search_OnClicked(object sender, EventArgs e)
+        {
+            var longtitude = double.Parse(Lon.Text);
+            var latitude = double.Parse(Lat.Text);
+
+            var url = string.Format("http://api.geonames.org/findNearByWeatherJSON?lat={0}&lng={1}&username=webmasterdevlin", latitude, longtitude);
+            await vm.GetWeatherAsync(url);
+        }
+    }
+}
